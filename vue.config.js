51044951da8514cf98f550af126d65ab976c02f1
sogrey <suzhaoheng2006@163.com>
2022-03-27 00:15:41 +0800

    💖 fix dependencies.

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,14 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Element Admin' // page title
+const name = defaultSettings.title || 'vue Admin Template' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
 // For example, Mac: sudo npm run
-// You can change the port by the following method:
-// port = 9527 npm run dev OR npm run dev --port = 9527
-const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+// You can change the port by the following methods:
+// port = 9528 npm run dev OR npm run dev --port = 9528
+const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -38,6 +38,24 @@ module.exports = {
     },
     before: require('./mock/mock-server.js')
   },
+  // devServer: {
+  //   host: 'localhost',//target host
+  //   port: 8080,
+  //   //proxy:{'/api':{}},代理器中设置/api,项目中请求路径为/api的替换为target
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://192.168.1.30:8085',//代理地址，这里设置的地址会代替axios中设置的baseURL
+  //       changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
+  //       //ws: true, // proxy websockets
+  //       //pathRewrite方法重写url
+  //       pathRewrite: {
+  //         '^/api': '/'
+  //         //pathRewrite: {'^/api': '/'} 重写之后url为 http://192.168.1.16:8085/xxxx
+  //         //pathRewrite: {'^/api': '/api'} 重写之后url为 http://192.168.1.16:8085/api/xxxx
+  //       }
+  //     }
+  //   }
+  // },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
@@ -49,7 +67,6 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
     // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
       {
@@ -88,7 +105,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
